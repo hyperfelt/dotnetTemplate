a4bac725c88576399e8db20a0095ef6457f5a948
Nicolas FOSSE <112903838+hyperfelt@users.noreply.github.com>
2024-04-21 11:27:49 +0200

    Implement logging with an attribute, following the aspect-oriented programming principle

--- dotnetTemplate.Persistence/Repositories/BaseRepository.cs
+++ dotnetTemplate.Persistence/Repositories/BaseRepository.cs
@@ -1,7 +1,9 @@
+using dotnetTemplate.Core.Attributes;
 using dotnetTemplate.Core.Interfaces.Repositories;
 
 namespace dotnetTemplate.Persistence.Repositories;
 
+[LogAndMeasure]
 public abstract class BaseRepository : IBaseRepository
 {
     
