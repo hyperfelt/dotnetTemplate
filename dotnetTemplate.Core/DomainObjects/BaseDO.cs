7ae64a91c4fc9001bfe5b4c2d772b41753d51aba
Nicolas FOSSE <112903838+hyperfelt@users.noreply.github.com>
2024-04-28 11:07:01 +0200

    Implement BaseDO with common properties

--- dotnetTemplate.Core/DomainObjects/BaseDO.cs
+++ dotnetTemplate.Core/DomainObjects/BaseDO.cs
@@ -1,3 +1,30 @@
 namespace dotnetTemplate.Core.DomainObjects;
 
-public abstract record BaseDO(int Id = -1);
+public abstract record BaseDO
+{
+    private int _id;
+    public int Id
+    {
+        get => _id;
+        set
+        {
+            if (value <= 0)
+                throw new Exception("The identifier must be a number superior to 0.");
+
+            _id = value;
+        }
+    }
+
+    private readonly DateTime _creationDate = DateTime.Now;
+    public DateTime CreationDate
+    {
+        get => _creationDate;
+        init
+        {
+            if (value > DateTime.Now)
+                throw new Exception("The creation date and time must be equal or lower to the current.");
+
+            _creationDate = value;
+        }
+    }
+}
